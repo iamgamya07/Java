4c801ce1042ea8c6eac69ec5bc435836afc4ded5
iamgamya07 <gamya.k@msam.christuniversity.in>
2024-11-13 10:44:02 +0530

    Create Robber.java
    Abstract Class Robber:
    
    Contains abstract methods (RowHouses, RoundHouses, SquareHouse, MultiHouseBuilding) to be implemented by subclasses.
    MachineLearning method prints "I love MachineLearning."
    RobbingClass method prints "MScAI&ML."

---
+++ Robber.java
@@ -0,0 +1,12 @@
+abstract class Robber {
+
+    // Default method
+    void MachineLearning() {
+        System.out.println("I love MachineLearning.");
+    }
+
+    // Function to print "MScAI&ML"
+    void RobbingClass() {
+        System.out.println("MScAI&ML");
+    }
+}
