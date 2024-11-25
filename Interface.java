bb75d5ee3fe8abe59ac9a91ef1a41e6de88641b5
iamgamya07 <gamya.k@msam.christuniversity.in>
2024-11-25 10:23:49 +0530

    Create Interface.java
    This Java program demonstrates the use of an interface to model bank operations.
    
    Interface (BankInterface): Defines methods getBalance() and getInterestRate() for retrieving account balance and interest rate.
    Concrete Classes (BankA, BankB, BankC): Implement the interface with specific balance and interest rate values for each bank.
    Main Class: Creates instances of the bank classes and prints their balances and interest rates.

---
+++ Interface.java
@@ -0,0 +1,81 @@
+interface BankInterface {
+    double getBalance();
+    double getInterestRate();
+}
+
+class BankA implements BankInterface {
+    private double balance;
+
+    public BankA(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public double getBalance() {
+        return balance;
+    }
+
+    @Override
+    public double getInterestRate() {
+        return 7.0;
+    }
+}
+
+class BankB implements BankInterface {
+    private double balance;
+
+    public BankB(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public double getBalance() {
+        return balance;
+    }
+
+    @Override
+    public double getInterestRate() {
+        return 7.4;
+    }
+}
+
+class BankC implements BankInterface {
+    private double balance;
+
+    public BankC(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public double getBalance() {
+        return balance;
+    }
+
+    @Override
+    public double getInterestRate() {
+        return 7.9;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        BankA bankA = new BankA(10000);
+        BankB bankB = new BankB(150000);
+        BankC bankC = new BankC(200000);
+
+    
+        System.out.println("Bank A:");
+        System.out.println("Balance: " + bankA.getBalance());
+        System.out.println("Interest Rate: " + bankA.getInterestRate() + "%");
+
+        System.out.println("\nBank B:");
+        System.out.println("Balance: " + bankB.getBalance());
+        System.out.println("Interest Rate: " + bankB.getInterestRate() + "%");
+
+        System.out.println("\nBank C:");
+        System.out.println("Balance: " + bankC.getBalance());
+        System.out.println("Interest Rate: " + bankC.getInterestRate() + "%");
+    }
+}
+
